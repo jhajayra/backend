dcdad1381aaf9d4c6a441e4a7ec826519f8caaf2
jhajayra <jhajayra95@gmail.com>
2023-02-23 22:52:35 -0300

    repository agregad

--- src/main/java/com/jhajayramori/app/security/repository/iUsuarioRepository.java
+++ src/main/java/com/jhajayramori/app/security/repository/iUsuarioRepository.java
@@ -3,10 +3,11 @@ package com.jhajayramori.app.security.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.jhajayramori.app.security.entity.User;
 
-
+@Repository
 public interface iUsuarioRepository extends JpaRepository<User, Integer>{
     Optional<User> findByNombreUsuario(String nombreUsuario);
     
